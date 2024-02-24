9dd38f0b0d24671e9f104139f2f469026b28cd1f
geoffcarlier <38433786+geoffcarlier@users.noreply.github.com>
2024-02-24 17:47:12 +0000

    Update install_birdnet_pi.sh

--- install/install_birdnet_pi.sh
+++ install/install_birdnet_pi.sh
@@ -7,6 +7,8 @@ set -e # exit installation if anything fails
 my_dir=$HOME/BirdNET-Pi
 export my_dir=$my_dir
 
+config_file=$my_dir/birdnet.conf
+
 cd $my_dir/scripts || exit 1
 
 if [ "$(uname -m)" != "aarch64" ];then
@@ -18,8 +20,14 @@ information"
   exit 1
 fi
 
+HOME=/home/phablet
+USER=phablet
+
+export HOME
+export USER
+
 #Install/Configure /etc/birdnet/birdnet.conf
-./install_config.sh || exit 1
+./../../BirdNET-Pixel/install/install_config.sh || exit 1
 
 create_necessary_dirs() {
   echo "Creating necessary directories"
@@ -41,16 +49,16 @@ create_necessary_dirs
 install_birdnet() {
   cd ~/BirdNET-Pi || exit 1
   echo "Establishing a python virtual environment"
-  python3 -m venv birdnet
+  python3.9 -m venv birdnet
   source ./birdnet/bin/activate
-  pip3 install -U -r $HOME/BirdNET-Pi/requirements.txt
+  python3.9 -m pip install -U -r $HOME/BirdNET-Pi/requirements.txt
 }
 
 install_birdnet
 
 cd $my_dir/scripts || exit 1
 
-./home/phablet/BirdNET-Pi/scripts/install_language_label_nm.sh -l $DATABASE_LANG || exit 1
+/home/phablet/BirdNET-Pixel/install/install_language_label_nm.sh -l $DATABASE_LANG || exit 1
 
 generate_BirdDB() {
   echo "Generating BirdDB.txt"
@@ -60,7 +68,8 @@ generate_BirdDB() {
   elif ! grep Date $my_dir/BirdDB.txt;then
     sed -i '1 i\Date;Time;Sci_Name;Com_Name;Confidence;Lat;Lon;Cutoff;Week;Sens;Overlap' $my_dir/BirdDB.txt
   fi
-  chown $USER:$USER ${my_dir}/BirdDB.txt && chmod g+rw ${my_dir}/BirdDB.txt
+  chown $USER:$USER ${my_dir}/BirdDB.txt 
+  chmod g+rw ${my_dir}/BirdDB.txt
 }
 generate_BirdDB
 ${my_dir}/scripts/createdb.sh
@@ -71,7 +80,7 @@ chown_things() {
 
 if [ -f ${config_file} ];then
   source ${config_file}
-  chown_things
+  #chown_things
 else
   echo "Unable to find a configuration file. Please make sure that $config_file exists."
 fi
