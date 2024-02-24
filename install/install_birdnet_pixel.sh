3b336282058fa99a5c1c9ae9ede489f378ac5c84
geoffcarlier <38433786+geoffcarlier@users.noreply.github.com>
2024-02-24 17:47:37 +0000

    Update install_birdnet_pixel.sh

--- install/install_birdnet_pixel.sh
+++ install/install_birdnet_pixel.sh
@@ -8,15 +8,19 @@ BIRDNET_DOC_HOME=~/Documents
 BIRDNET_PIXEL_HOME=$BIRDNET_DOC_HOME/.birdnet
 BIRDNET_DATA=$BIRDNET_DOC_HOME/BirdnetData
 BIRDNET_INSTALL_SCRIPTS=$BIRDNET_PIXEL_HOME/install
+BIRDNET_SERVICES=$BIRDNET_PIXEL_HOME/services
 
 echo "Creating necessary directories"
 [ -d ${BIRDNET_DATA} ] || mkdir -p ${BIRDNET_DATA}
 [ -d ${BIRDNET_PIXEL_HOME} ] || mkdir -p ${BIRDNET_PIXEL_HOME}
-[ -d ${BBIRDNET_INSTALL_SCRIPTS} ] || mkdir -p ${BIRDNET_INSTALL_SCRIPTS}
+[ -d ${BIRDNET_INSTALL_SCRIPTS} ] || mkdir -p ${BIRDNET_INSTALL_SCRIPTS}
+[ -d ${BIRDNET_SERVICES} ] || mkdir -p ${BIRDNET_SERVICES}
 
-cp /home/phablet/BirdNET-Pixel/install/install_services.sh BIRDNET_INSTALL_SCRIPTS
+cp /home/phablet/BirdNET-Pixel/install/install_services.sh ${BIRDNET_INSTALL_SCRIPTS}
+cp -r /home/phablet/BirdNET-Pixel/services/* ${BIRDNET_SERVICES}
 
-chmod -R g+rw $my_dir
-chmod -R g+rw ${RECS_DIR}
+chmod g+rx ${BIRDNET_INSTALL_SCRIPTS}
+chmod g+rw ${BIRDNET_SERVICES}/*
+chmod g+rx ${BIRDNET_SERVICES}/bin/*
 
 exit 0
